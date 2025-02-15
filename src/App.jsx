5ac8a6778c5eb410d68036a65f66c65034581ae2
jinggu0 <yoonhyuk1208@gmail.com>
2025-02-16 02:49:02 +0900

    route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
 	return (
@@ -8,6 +10,8 @@ function App() {
 			<Route path='/' element={<Home />} />
 			<Route path='/mypage' element={<p>mypage</p>} />
 			<Route path='/detail/:id' element={<Detail />} />
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
 		</Routes>
 	);
 }
